eb3bf9519a7ab98cec0f726449307d1e4142fe8b
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-03-06 17:42:10 -0500

    changed API routers to use username rather than ID

--- server/app/routes/api/users.js
+++ server/app/routes/api/users.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose');
 require('../../../db/models/user');
 var User = mongoose.model('User');
 
-router.use('/:userId/reviews', require('./reviews'));
+router.use('/:username/reviews', require('./reviews'));
 
-router.param('userId', function (req, res, next, userId){
-  User.findById(userId)
+router.param('username', function (req, res, next, username){
+  User.findOne({username: username})
   .then(function(user){
     req.reqUser = user;
     next();
@@ -28,18 +28,24 @@ router.get('/', function (req, res, next) {
 })
 
 // only available to admin and user themself?
-router.get('/:userId', function (req, res) {
+router.get('/:username', function (req, res) {
   res.json(req.reqUser.sanitize())
 })
 
 // admin and user
 // is this already taken care of in orders route??
-router.get('/:userId/orders', function (req, res, next) {
-  req.reqUser.findOrders()
-  .then(function(orders) {
-    res.json(orders)
-  })
-  .then(null, next)
+router.get('/:username/orders', function (req, res, next) {
+  if (req.reqUser._id === req.user.id || req.user.isAdmin) {
+    req.reqUser.findOrders()
+    .then(function(orders) {
+      res.json(orders)
+    })
+    .then(null, next)
+  } else {
+    var err = new Error();
+    err.status = 403;
+    next(err);
+  }
 })
 
 // post a new user
@@ -52,22 +58,34 @@ router.post('/', function (req, res, next) {
 })
 
 // update a user
-router.put('/:userId', function (req, res, next) {
-  req.reqUser.set(req.body)
-  req.reqUser.save()
-  .then(function(updatedUser){
-    res.json(updatedUser);
-  })
-  .then(null, next);
+router.put('/:username', function (req, res, next) {
+  if (req.reqUser._id === req.user || req.user.isAdmin) {
+    req.reqUser.set(req.body)
+    req.reqUser.save()
+    .then(function(updatedUser){
+      res.json(updatedUser);
+    })
+    .then(null, next);
+  } else {
+    var err = new Error();
+    err.status = 403;
+    next(err);
+  }
 })
 
 // delete a user
-router.delete('/:userId', function (req, res, next) {
-  req.reqUser.remove()
-  .then(function(){
-    res.sendStatus(204)
-  })
-  .then(null, next);
+router.delete('/:username', function (req, res, next) {
+  if (req.reqUser._id === req.user || req.user.isAdmin) {
+    req.reqUser.remove()
+    .then(function(){
+      res.sendStatus(204)
+    })
+    .then(null, next);
+  } else {
+    var err = new Error();
+    err.status = 403;
+    next(err);
+  }
 })
 
 
