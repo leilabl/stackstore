1596228b4ad153d2dbe1d7390d9cdec5ccf28ae9
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-03-06 17:43:00 -0500

    add table with user stats and list of reviews

--- browser/js/users/user.js
+++ browser/js/users/user.js
@@ -1,11 +1,11 @@
 app.config(function($stateProvider){
   $stateProvider.state('user', {
-    url: '/users/:userId',
+    url: '/users/:username',
     templateUrl: 'js/users/user.html',
     controller: 'UserController',
     resolve: {
       user: function(UserFactory, $stateParams) {
-        return UserFactory.getUser($stateParams.userId);
+        return UserFactory.getUser($stateParams.username);
       }
     }
   })
@@ -14,8 +14,8 @@ app.config(function($stateProvider){
 app.factory('UserFactory', function($http, $q){
   var UserFactory = {};
 
-  UserFactory.getUser = function(id){
-    var userUrl = '/api/users/' + id;
+  UserFactory.getUser = function(username){
+    var userUrl = '/api/users/' + username;
     var reviewsUrl = userUrl + '/reviews';
     return $q.all([$http.get(userUrl), $http.get(reviewsUrl)])
       .then(function(responses){
