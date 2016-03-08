471ac391a0a7cb2f665fc48473581a66d65ae493
Alexandra Polubiec <apolubi@Alexandras-MacBook-Pro.local>
2016-03-07 22:21:29 -0500

    add cart lookup

--- browser/js/cart/cart.js
+++ browser/js/cart/cart.js
@@ -2,15 +2,30 @@ app.config(function($stateProvider) {
   $stateProvider.state('cart', {
     url: '/cart',
     templateUrl: 'js/cart/cart.html',
-    controller: 'CartCtrl'
+    controller: 'CartCtrl',
+    resolve: {
+      contents: function(AuthService, CartFactory) {
+        return CartFactory.getCart(AuthService.getLoggedInUser());
+      }
+    }
   })
 })
 
-app.factory('CartFactory', function() {
+app.factory('CartFactory', function(localStorageService) {
+  var CartFactory = {};
+
+  CartFactory.getCart = function (user) {
+    if (!user) {
+      return localStorageService.get('cart');
+    } else {
+      return $http.get('/api/users/' + user._id + 'cart')
+        .then(function(res) {
+          return res.data;
+        });
+    }
+  }
 
 })
-
-//localStorageService
 
 myApp.controller('CartCtrl', function($scope, localStorageService) {
 
@@ -22,7 +37,7 @@ myApp.controller('CartCtrl', function($scope, localStorageService) {
       localStorageService.set('cart', item);
     }  
   }
-  
+
   // we need to make sure to run localStorageService.$reset() after checkout
 
 });
